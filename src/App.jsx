79a5b2673b3385a4feda72eabd5a9c92be4f0928
Chris <chye0001@stud.kea.dk>
2024-11-26 21:22:03 +0100

    Created 2 DivEx Components: StocksPaginated and PaginationBar, also added some quick test data to StocksPaginated

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,25 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "@/components/theme-provider";
 import Homepage from "@/pages/homepage";
 import ModeToggle from "@/components/ui/mode-toggle";
-
+import StocksPaginated from "@/components/divex/StocksPaginated"
 
 function App() {
-  
+
 
   return (
     <>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      
-      <ModeToggle />
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-      </Routes>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+
+        <ModeToggle />
+        <Navbar />
+        <Router>
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/stocks" element={<StocksPaginated />}></Route>
+          </Routes>
         </Router>
-        </ThemeProvider>
+        
+      </ThemeProvider>
     </>
   );
 }

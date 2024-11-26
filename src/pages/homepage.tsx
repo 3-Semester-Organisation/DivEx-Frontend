b9844bf569e808d2a12e60c2cb9223a478800943
madsfjeldberg <mafj0001@stud.kea.dk>
2024-11-26 09:44:27 +0100

    added styling and comps for shadcn, light/dark mode toggle

---
+++ src/pages/homepage.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export default function homepage() {
+    return (
+    <>
+    <div>
+        <h1>Homepage</h1>
+        <p>Welcome to the homepage</p>
+    </div>
+    </>
+)
+}

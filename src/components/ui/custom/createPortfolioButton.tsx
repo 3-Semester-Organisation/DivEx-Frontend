ecf339174cc48e763f0667f1b92cbd6d2296f786
madsfjeldberg <mafj0001@stud.kea.dk>
2024-12-02 18:02:42 +0100

    select and create portfolio done

---
+++ src/components/ui/custom/createPortfolioButton.tsx
@@ -0,0 +1,97 @@
+import * as React from "react";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  portfolioName: z
+    .string()
+    .min(1, { message: "Name must be at least 1 character long" }),
+});
+
+export function CreatePortfolioButton({ onSubmit }) {
+  const [open, setOpen] = React.useState(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      portfolioName: "",
+    },
+  });
+
+  const handleSubmit = async (values: z.infer<typeof formSchema>) => { 
+    await onSubmit(values)
+    setOpen(false)
+  }
+
+  return (
+    <>
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
+          <Button className="bg-accent-foreground">Create portfolio</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(handleSubmit)}>
+              <DialogHeader>
+                <DialogTitle>Create portfolio</DialogTitle>
+                <DialogDescription>Create a new portfolio.</DialogDescription>
+              </DialogHeader>
+              <div className=" gap-4 py-4">
+                <div className="items-center gap-4">
+                  <FormField
+                    control={form.control}
+                    name="portfolioName"
+                    render={({ field }) => (
+                      <FormItem className="grid gap-2">
+                        <FormLabel
+                          className="flex justify-between items-center"
+                          htmlFor="portfolioName"
+                        >
+                          Portfolio name
+                        </FormLabel>
+                        <FormControl>
+                          <Input
+                            id="portfolioName"
+                            type="portfolioName"
+                            autoComplete="portfolioName"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+              </div>
+              <DialogFooter>
+                <Button type="submit">Create</Button>
+              </DialogFooter>
+            </form>
+          </Form>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+}

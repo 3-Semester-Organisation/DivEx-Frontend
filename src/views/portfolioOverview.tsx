ce5f0a58fe35c37e0c6253ed771980bd28189a49
Chris <chye0001@stud.kea.dk>
2024-12-11 12:25:02 +0100

    created function for summirizing same multi port entries. Also added new state summirizedPortfolio

--- src/views/portfolioOverview.tsx
+++ src/views/portfolioOverview.tsx
@@ -19,7 +19,7 @@ import { Button } from "@/components/ui/button";
 
 import useCheckCredentials from "@/js/useCredentials";
 import DividendSummaryTable from "@/components/divex/DividendSummaryTable";
-import { Portfolio } from "@/divextypes/types";
+import { Portfolio, PortfolioEntry } from "@/divextypes/types";
 import { getSubscriptionTypeFromToken } from "@/js/jwt";
 import { SquareArrowOutUpRight } from "lucide-react";
 import DividendBarChart from "@/components/divex/DividendBarChart";
@@ -34,8 +34,68 @@ export default function PortfolioOverview() {
   const [currency, setCurrency] = useState("DKK");
   const supportedCurrencies: string[] = ["DKK", "SEK", "NOK"];
   const [isDisplayingDividendSummary, setIsDisplayingDividendSummary] = useState(false);
+  const [summarizedPortfolio, setSummarizedPortfolio] = useState<Portfolio>(null)
 
   useCheckCredentials();
+
+  function summarizePortfolioEntries() {
+
+    if (selectedPortfolio === null) {
+      console.log("NOT")
+      return;
+    }
+    console.log("HAS RUN")
+    const summarizedEntries = [];
+    let totalCost = 0;
+    let totalNumberOfShares = 0;
+    let avgAcquiredPrice = 0;
+    let previousEntry = null;
+
+    const portfolioEntries = [...selectedPortfolio.portfolioEntries];
+    portfolioEntries.sort((a, b) => a.stock.name.localeCompare(b.stock.name));
+
+    for (const currentEntry of portfolioEntries) {
+
+      if (previousEntry !== null && previousEntry.stock.name.toLowerCase() === currentEntry.stock.name.toLowerCase()) {
+        totalCost += currentEntry.stockPrice * currentEntry.quantity;
+        totalNumberOfShares += currentEntry.quantity;
+      } else {
+
+        if (previousEntry !== null) {
+          avgAcquiredPrice = totalCost / totalNumberOfShares;
+          summarizedEntries.push({
+            ...previousEntry,
+            avgAcquiredPrice: avgAcquiredPrice.toFixed(2),
+            quantity: totalNumberOfShares,
+          });
+        }
+
+        previousEntry = currentEntry;
+        totalCost = currentEntry.stockPrice * currentEntry.quantity;
+        totalNumberOfShares = currentEntry.quantity;
+      }
+    }
+
+    if (previousEntry !== null) {
+      avgAcquiredPrice = totalCost / totalNumberOfShares;
+      summarizedEntries.push({
+        ...previousEntry,
+        avgAcquiredPrice: avgAcquiredPrice.toFixed(2),
+        quantity: totalNumberOfShares,
+      });
+    }
+
+    const updatedPortfolio = {
+      ...selectedPortfolio,
+      portfolioEntries: summarizedEntries,
+    };
+
+    // setSelectedPortfolio(updatedPortfolio);
+    setSummarizedPortfolio(updatedPortfolio);
+
+    console.log("Updated Portfolios:", updatedPortfolio);
+  }
+
 
   async function handlePortfolioCreation(values) {
     const newPortfolio = await createPortfolio(values);
@@ -65,7 +125,10 @@ export default function PortfolioOverview() {
 
     loadPortfolios();
     setSelectedPortfolio(selectedPortfolio);
+    console.log(selectedPortfolio)
   }, []);
+
+  useEffect(() => { summarizePortfolioEntries(); console.log("SELECTED", selectedPortfolio); console.log("SUMMARIZED", summarizedPortfolio) }, [selectedPortfolio])
 
   const changePortfolioName = async (newName: string) => {
     const token = localStorage.getItem("token");
@@ -119,7 +182,7 @@ export default function PortfolioOverview() {
       </div>
 
       <div className="flex flex-row content-center gap-3 pt-5">
-        <div className="">
+        <div>
           {selectedPortfolio && (
             <PortfolioSelect
               portfolioList={portfolios}
@@ -129,14 +192,14 @@ export default function PortfolioOverview() {
           )}
         </div>
 
-        <div className="">
+        <div>
           <CreatePortfolioButton
             onSubmit={handlePortfolioCreation}
             portfolios={portfolios}
           />
         </div>
 
-        <div className="">
+        <div>
           {selectedPortfolio && (
             <>
               <Button
@@ -193,27 +256,28 @@ export default function PortfolioOverview() {
         {selectedPortfolio && (
           <div className="grid grid-cols-12">
             <div className="col-span-4 mt-5">
-              <PortfolioChart selectedPortfolio={selectedPortfolio} />
+              <PortfolioChart selectedPortfolio={summarizedPortfolio} />
             </div>
 
             <div className="col-span-8 mt-5">
-            {isDisplayingDividendSummary && (
-              <DividendBarChart 
-              currency={currency} />
+              {isDisplayingDividendSummary && (
+                <DividendBarChart
+                  selectedPortfolio={summarizedPortfolio}
+                  currency={currency} />
               )}
             </div>
 
             <div className="col-span-12">
               {isDisplayingDividendSummary ? (
                 <DividendSummaryTable
-                  selectedPortfolio={selectedPortfolio}
-                  setSelectedPortfolio={setSelectedPortfolio}
+                  selectedPortfolio={summarizedPortfolio}
+                  setSelectedPortfolio={setSummarizedPortfolio}
                   currency={currency}
                   numberFormater={numberFormater}
                 />) : (
                 <PortfolioTable
-                  selectedPortfolio={selectedPortfolio}
-                  setSelectedPortfolio={setSelectedPortfolio}
+                  selectedPortfolio={summarizedPortfolio}
+                  setSelectedPortfolio={setSummarizedPortfolio}
                   currency={currency}
                   numberFormater={numberFormater}
                 />)}

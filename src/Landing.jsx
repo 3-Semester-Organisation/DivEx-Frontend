15e07ca8d1232167e46eabc426434e6f72e3486f
Lars <144362461+laha0006@users.noreply.github.com>
2024-12-13 09:21:59 +0100

    landing page component

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,20 +1,19 @@
-import {Boxes} from "@/components/ui/background-boxes"
+// import {Boxes} from "@/components/ui/background-boxes"
+import {BackgroundLines} from "@/components/ui/background-lines";
 
 
 export default function Landing() {
     return (
-        <>
-            <div>
-                <Boxes/>
-                <div className="container">
-                    <h1 className="font-semibold text-9xl relative">DivEx</h1>
-                    <a className="relative" href="/app">GO TO APP</a>
-                </div>
+        <div className="flex flex-col">
+            <div className="">
+                <h1 className="font-semibold text-9xl z-20">DivEx</h1>
+                <p className="text-2xl">With DivEx you will <insert> your portfolio</insert></p>
+                <a className="relative z-20" href="/app">GO TO APP</a>
             </div>
-            <div className="h-">
-
+            <div className="">
+                <h1>Test</h1>
             </div>
-        </>
+        </div>
     )
 }
 

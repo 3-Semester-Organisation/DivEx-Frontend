0c479363a9a823e64b8b6a21710668cd65e3b80c
madsfjeldberg <mafj0001@stud.kea.dk>
2024-12-15 14:50:17 +0100

    removed commented out code

--- src/App.jsx
+++ src/App.jsx
@@ -58,52 +58,8 @@ function Layout({ children, isLoggedIn }) {
 
 function App() {
   // gets login state from AuthContext
-  const { isLoggedin: isLoggedIn } = useContext(AuthContext);
-
-  {
-    /*
-  return (
-    <div className="flex flex-col min-h-screen">
-      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <header className="flex justify-between">
-      <div className="mb-2">
-        <div className="flex justify-start items-center">
-          <img src="/divex-icon-no-bg.png" alt="DivEx-icon" width="30px" height="30px"/>  
-          <h1 className="font-semibold text-2xl ml-1">DivEX</h1>
-        </div>  
-          </div>
-          </header>
-      
-      <Router>
-          {isLoggedIn ? <Navbar /> : <DefaultNavbar />}
-          <main className="flex-grow">
-        <Routes>
-          <Route path="/" element={<p>homepage</p>} />
-          <Route path="/trending" element={<Trending />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/pricing" element={<Pricing />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/account" element={<p>account</p>} />
-          <Route path="/settings" element={<Settings />} />
-          <Route path="/calendar" element={<CalendarPage />} />
-          <Route path="/stocks" element={<StocksPage />} />
-          <Route path="/upgrade" element={<CardInput />} />
-
-          <Route path="/portfolio/overview" element={<PortfolioOverview isLoggedIn={isLoggedIn} />} />
-          <Route path="/stocks/:ticker" element={<StockDetailsPage />} />
-
-          <Route path="*" element={<NotFoundView />} />
-            </Routes>
-          </main>
-        <Footer />
-      </Router>
-      </ThemeProvider>
-      </div>
+    const { isLoggedin: isLoggedIn } = useContext(AuthContext);
     
-  );
-  */
-  }
-
   return (
     <div className="flex flex-col min-h-screen">
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
